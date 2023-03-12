98a94fae207e16e58fc71b8ba9e1864f9c70c61d
okeyoo <126993560+okeyoo@users.noreply.github.com>
2023-03-12 03:12:23 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+const togglePassword = bitton => {
+
+		button.classList.toggle("showing")
+
+	const input =
+		document.getElementById("password")
+
+	input.type =
+		input.type === "password"
+		? "text"
+		: "password"
+}
